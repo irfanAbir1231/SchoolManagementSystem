62d2d5fd258180175b14d6f091703efe95712756
irfanAbir1231 <irfahakim@iut-dhaka.edu>
2024-12-02 00:50:57 +0600

    Initial commit

---
+++ src/Student.java
@@ -0,0 +1,34 @@
+public class Student {
+    private int id;
+    private String name;
+    private int grade;
+    private int feesPaid;
+    private int feesTotal;
+
+    public Student(int id, String name, int grade) {
+        this.feesPaid = 0;
+        this.feesTotal = 30000;
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    //not going to alter student name and id.
+
+    /**
+     * used to update student's grade
+     * @param grade new grade of the student's
+     */
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    /**
+     * add the fees to the fees paid
+     * the school is going to receive the funds.
+     * @param feesPaid is the fees that the student pays
+     */
+    public void updateFeesPaid(int feesPaid) {
+        this.feesPaid += feesPaid;
+    }
+}
---
+++ src/Teacher.java
@@ -0,0 +1,29 @@
+public class Teacher {
+    private String name;
+    private int id;
+    private int salary;
+
+    public Teacher(String name, int id, int salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    /**
+     *
+     * @return the name of the current teacher
+     */
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+
+}
