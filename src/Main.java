166d5cba2acf3dee67cfe13e448a01b36c62187c
irfanAbir1231 <irfahakim@iut-dhaka.edu>
2024-12-05 23:06:34 +0600

    tested arraylist 2

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,9 @@ public class Main {
         numbers.add(1);
         numbers.add(2);
         numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
 
         System.out.println(numbers);
     }
