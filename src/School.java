cd2b79e8e7173032477016bd3c4f891d3c41ac89
irfanAbir1231 <irfahakim@iut-dhaka.edu>
2024-12-05 22:57:11 +0600

    tested arraylist

---
+++ src/School.java
@@ -0,0 +1,12 @@
+public class School {
+    /**
+     * many teacher, many students
+     * its not good ...doing...teacher1, teacher2, teacher2....
+     * we can also create an array of teacher or students but ...we dont know the count
+     * so we can implement student and teacher using arraylist
+     */
+    private Teacher teacher;
+    private Student student;
+    private int totalMoneyEarned;
+    private int totalMoneySpent;
+}
